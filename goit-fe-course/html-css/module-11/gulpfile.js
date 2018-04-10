1753c09faf6977bc70934874655f0dfe148d4a95
Ruslan Pashko <rus612.89@gmail.com>
2018-04-10 17:47:55 +0300

    goit-fe-course

---
+++ goit-fe-course/html-css/module-11/gulpfile.js
@@ -0,0 +1,21 @@
+﻿var gulp = require('gulp'),
+    sass = require('gulp-sass');
+	jade = require('gulp-jade');
+
+gulp.task('sass', function() {
+  return gulp.src(['src/sass/**/*.sass', 'src/sass/**/*.scss'])
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(gulp.dest('app/css'))
+  });
+
+gulp.task('watch', function() {
+  gulp.watch(['src/sass/**/*.sass', 'src/sass/**/*.scss'], ['sass']);
+});
+
+gulp.task('default', ['watch']);
+
+gulp.task('jade', function() {
+    return gulp.src('src/templates/**/*.jade')
+        .pipe(jade()) 
+        .pipe(gulp.dest('app/'));
+});
